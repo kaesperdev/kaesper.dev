bb48493382e07149b661862d0d0e04e7731c0c85
kaesperdev <hello@kaesper.dev>
2022-10-19 01:52:34 +0200

    :sparkles: Add why page

--- src/pages/Why.jsx
+++ src/pages/Why.jsx
@@ -1,5 +1,26 @@
 const Why = () => {
-  return <p>Why</p>;
+  return (
+    <section id='why'>
+      <h1 class='text-4xl font-bold md:text-5xl'>Why Kaesper? 🤔</h1>
+      <h2 class='mt-8 text-2xl font-normal md:mt-12 md:text-3xl'>
+        Simply put, I was looking for something that would meet these criteria:
+      </h2>
+      <ul class='mt-4 list-disc pl-10 text-xl md:text-2xl'>
+        <li>Relate to me in some way</li>
+        <li>Easy to remember & pronounce</li>
+        <li>Unique enough to not be taken on my most used platforms</li>
+      </ul>
+      <h2 class='mt-8 text-2xl font-normal md:mt-10 md:text-3xl'>
+        A longer explanation on how I came up with it:
+      </h2>
+      <h3 class='mt-4 text-xl md:text-2xl'>
+        The username itself comes from the phonetic transcription of the word
+        Casper (ˈkæspər), which is the anglicized form of my actual first name
+        (Kacper). It's also a reference to the ghost from the movie of the same
+        name, which I've always found to be quite funny.
+      </h3>
+    </section>
+  );
 };
 
 export default Why;
